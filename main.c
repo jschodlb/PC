80dec0d7abf33bf7f347417c73bc525d018ff59a
schodlbauer <jschodlb@students.zcu.cz>
2021-12-25 21:54:34 +0100

    Reading all pixels from a file and saves it into an array correctly

--- main.c
+++ main.c
@@ -1,91 +1,70 @@
-#include <stdio.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include <string.h>
 
-#define FAILURE 0
-#define SUCCESS 1
-
-int read_file(char *output[], char *file_name) {
-    char prefix[] = "..\\tests\\";
-    FILE *file;
-    size_t size = 0;
-    int i;
-
-    if (!file_name || !output)
-    {
-        printf("Failure when reading from file.");
-        return FAILURE;
-    }
-
-    strcat(prefix, file_name);
-    printf("%s\n", prefix);
-    file = fopen(prefix, "r");
-
-    if (!file)
-    {
-        printf("Failure when opening file.");
-        return FAILURE;
-    }
-
-    /* Get output size */
-    fseek(file, 0L, SEEK_END); // goes to the end of file
-    size = ftell(file); // how many bytes did it got through
-
-    rewind(file); // go to the beginning of file
-
-    output = calloc(1, (size+1) * sizeof(char)); // size + 1 for \0
-
-    fread(output, size, 1, file); // reads 1 times sizes of byte from file into output
-
-    output[size] = '\0';
-
-    printf("%s\n", output);
-
-
-    fclose(file);
-
-    return SUCCESS;
-}
-
-int number_of_lines(char *file_name) {
-    FILE *file;
-    char prefix[] = "..\\tests\\";
-    int lines = 0;
-    char character;
-
-    // Sanity check
-    if (!file_name)
-    {
-        printf("Failure when reading from file.");
-        return FAILURE;
-    }
-
-    strcat(prefix, file_name);
-    printf("%s\n", prefix);
-    file = fopen(prefix, "r");
-
-    // Cycle until end of file is found
-    while(!feof(file))
-    {
-        /* gets a character */
-        character = fgetc(file);
-        /* if a end of line is found, increment variable lines */
-        if(character == '\n')
-        {
-            lines++;
-        }
-    }
-
-    return lines;
+int get_index(int x, int y, int width) {
+    return y * width + x;
 }
 
 int main(int argc, char *argv[]) {
-    char *x[] = { 0 };
-    // koment
+    FILE *file;
+    char file_name[255];
+    char buff[255];
+    int width, height, max_value;
+    int i, j, index;
+    unsigned char pixel;
+    unsigned char *pixels;
 
-    // read_file(x, argv[1]);
+    strcpy(file_name, "..\\tests\\");
+    strcat(file_name, argv[1]);
 
-    printf("Number of lines: %d", number_of_lines(argv[1]));
+    file = fopen(file_name, "r");
+
+    if (!file)
+    {
+        printf("File not found.");
+        return EXIT_FAILURE;
+    }
+
+    fscanf(file, "%s", buff);
+
+    fscanf(file, "%d %d", &width, &height);
+    fscanf(file, "%d", &max_value);
+    // reading new line symbol, so it does not end up in pixels array
+    fscanf(file, "%c", &pixel);
+
+    printf("buff %s\nwidth %d\nheight %d\nmax value %d\n", buff, width, height, max_value);
+
+    pixels = calloc(width * height, sizeof(unsigned char));
+
+    index = 0;
+    for (i = 0; i < height; i++)
+    {
+        for (j = 0; j < width; j++)
+        {
+            fscanf(file, "%c", &pixel);
+
+            if (!((unsigned char) pixel == max_value || (unsigned char) pixel == 0))
+            {
+                printf("Incorrect PGM format.");
+                fclose(file);
+                free(pixels);
+                return EXIT_FAILURE;
+            }
+
+            pixels[index++] = (unsigned char) pixel;
+        }
+    }
+    fclose(file);
+
+    for (i = 0; i < height; i++)
+    {
+        for (j = 0; j < width; j++)
+        {
+            printf("%d", pixels[get_index(j, i, width)] == 255 ? 1 : 0);
+        }
+        printf("\n");
+    }
 
     return EXIT_SUCCESS;
 }
